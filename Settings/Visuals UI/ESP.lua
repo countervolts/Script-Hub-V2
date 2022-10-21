d7cb4bf537bc278b641e8480a8256ac80b205487
countervolts <93222141+countervolts@users.noreply.github.com>
2022-10-21 18:53:24 -0400

    Create ESP.lua

---
+++ Settings/Visuals UI/ESP.lua
@@ -0,0 +1,13 @@
+local function USTQEDP_fake_script() -- ESP.LocalScript 
+	local script = Instance.new('LocalScript', ESP)
+
+	getgenv().enabled = true --Toggle on/off
+	getgenv().filluseteamcolor = false --Toggle fill color using player team color on/off
+	getgenv().outlineuseteamcolor = false --Toggle outline color using player team color on/off
+	getgenv().fillcolor = Color3.new(0, 0, 0) --Change fill color, no need to edit if using team color
+	getgenv().outlinecolor = Color3.new(1, 1, 1) --Change outline color, no need to edit if using team color
+	getgenv().filltrans = 0 --Change fill transparency
+	getgenv().outlinetrans = 0 --Change outline transparency
+	
+	loadstring(game:HttpGet("https://pastebin.com/raw/KDWyMxNw"))()
+end
